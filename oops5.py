5dab30042316f06e280c4a5ef4ffacc855395f65
Kishan N <kishan01082005@gmail.com>
2026-08-28 20:28:44 +0530

    Added OOP inheritance examples

--- oops5.py
+++ oops5.py
@@ -1,17 +1,23 @@
-class shape:
+class Shape:
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+class Rectangle(Shape):
     def __init__(self, length, breadth):
         self.length = length
         self.breadth = breadth
 
-class circle(shape):
-    def area(self):
-        return 3.14 * (self.length ** 2)
-    
-class rectangle(shape):
     def area(self):
         return self.length * self.breadth
-    
-circle1 = circle(5, 0)
-print("Area of circle:", circle1.area())
-        
-     
+
+shapes = [Circle(5), Rectangle(10, 5)]
+
+for shape in shapes:
+    print("Area =", shape.area())
